fb43377bfb7f9eb676c25cfa395d72cb74146e5d
Leejun14 <777ljy777@gmail.com>
2026-04-01 13:07:19 +0900

    chore: init

---
+++ src/main/java/com/example/coffeeorderingproject/CoffeeOrderingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeorderingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderingProjectApplication.class, args);
+    }
+
+}
